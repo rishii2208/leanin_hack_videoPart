6f0cc81344e1ab193e5ca515b54d90fdf84cf058
rishi <rishirajrajapati22@gmail.com>
2025-02-23 11:56:43 +0530

    Reduced logo size and some minor chnagesx

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <img src="/black yellow minimalist Idea Logo.gif" alt="logo" className="mb-4" />
+      <img src="/logo.png" alt="logo" className="mb-4" width="300" height="300"/>
       <a
         href="/Video"
         className="px-4 py-2 bg-yellow-500 rounded hover:bg-yellow-600 transition"
